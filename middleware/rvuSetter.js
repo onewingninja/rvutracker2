8dd052610e1c5cbd0b9701451c5b8566fe2219c8
onewingninja <python60527@gmail.com>
2021-07-05 19:05:06 -0500

    rvu calc

--- middleware/rvuSetter.js
+++ middleware/rvuSetter.js
@@ -1,4 +1,27 @@
 
-module.exports = function(task, hospitalId){
-    
+const _ = import('lodash');
+const { Hospital } = require("../models/hospital");
+
+module.exports = function(log, hospitalId){
+    const task = log.task;
+    const hospital = Hospital.findById(hospitalId);
+
+    let rvuValue;
+
+    switch(!undefined){
+        case log.rvuReq:
+            rvuValue = log.rvuReq;
+            break;
+        case findCustom(log, hospital):
+            break;
+        case findDefault(log):
+            
+    }
+
+    async function findCustom(log, hospital){
+        const customValues = hospital.rvuSettings;
+        
+        return rvuValue = ( await _).find(customValues, (v) => { return v.name == log.task});
+    }
+
 }
